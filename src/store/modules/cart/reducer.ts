e6a74534a020234abd58e472bcba8e09cc73b0d3
bruno-nakahara <Bruno_Nakahara95@yahoo.com>
2021-07-17 20:43:28 -0300

    add actions

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,15 +1,37 @@
 import { Reducer } from "redux";
+import produce from 'immer';
 import { ICartState } from "./types";
 
 const INITIAL_STATE: ICartState = {
     items: []
 };
 
-const cart: Reducer<ICartState> = (state, action) => {
-    console.log(state)
-    console.log(action)
+const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
+    return produce(state, draft => {
+        switch(action.type) {
+            case 'ADD_PRODUCT_TO_CART': {
+                const { product } = action.payload;
 
-    return INITIAL_STATE;
+                const productInCartIndex = draft.items.findIndex(item => 
+                    item.product.id === product.id    
+                )
+
+                if (productInCartIndex >= 0) {
+                    draft.items[productInCartIndex].quantity++;
+                } else {
+                    draft.items.push({
+                        product,
+                        quantity: 1,
+                    })
+                }
+
+                break;
+            }
+            default: {
+                return draft; 
+            }
+        }
+    });
 };
 
 export default cart;
