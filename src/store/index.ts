f87221be41fa0cfcfc54f909b650ed3500ebce6e
bruno-nakahara <Bruno_Nakahara95@yahoo.com>
2021-07-19 21:09:59 -0300

    redux saga

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,20 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { ICartState } from './modules/cart/types';
+import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './modules/rootReducer';
+import rootSaga from './modules/rootSaga';
 
 export interface IState {
     cart: ICartState;
 }
 
-const store = createStore(rootReducer);
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
+
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
